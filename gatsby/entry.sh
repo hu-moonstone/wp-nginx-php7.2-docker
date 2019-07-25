c094a2a1c59063b93ef4f7c65f946180ca2f2921
hu.moonstone <hu.moonstone@gmail.com>
2019-07-25 15:56:18 +0900

    Add gatsby

---
+++ gatsby/entry.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+export PATH=$PATH:/usr/local/bin/gatsby
+
+# if [ "$1" == "serve-nginx" ]; then
+#     nginx -g "daemon off;"
+# else
+# #    gatsby telemetry --disable
+# #    gatsby develop -H 0.0.0.0 --port 8000 &
+# fi
